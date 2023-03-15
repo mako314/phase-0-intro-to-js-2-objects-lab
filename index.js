c1ce162b90f512b7e968c9d5a6711efcd577511c
Macolister Bispo <bispo.mac@gmail.com>
2023-03-15 16:33:14 -0400

    commit tree 6e8d6ab3b02f048cf16728ef01b49d08c419cd6f

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+const employee = {
+    name: "Marc Jacobs",
+    address: "1234 Whoville Street",
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {return {
+    ...employee,
+    [key]: value,
+    };
+};
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+};
+
+function deleteFromEmployeeByKey(employee, name,) {
+    const newEmployee = {employee};
+    delete newEmployee.name;
+
+    return newEmployee;
+};
+
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee[name];
+    return employee;
+};
